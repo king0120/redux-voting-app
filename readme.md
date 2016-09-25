032fc228122ac02a0600add97c2f3c45b444d591
Jamie King <king0120@gmail.com>
2016-09-25 14:24:23 -0400

    built app

---
+++ readme.md
@@ -0,0 +1,5 @@
+#Redux/React Voting App
+
+Fullstack React app using ES6, Babel, Webpack, Redux, React, and Socket.io
+
+Created following http://teropa.info/blog/2015/09/10/full-stack-redux-tutorial.html
---
+++ voting-client/src/index.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Router, Route, hashHistory} from 'react-router';
+
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import reducer from './reducer';
+import {setState} from './action_creators';
+import remoteActionMiddleware from './remote_action_middleware';
+
+import io from 'socket.io-client';
+import App from './components/App';
+import {VotingContainer} from './components/Voting.jsx';
+import {ResultsContainer} from './components/Results.jsx';
+
+const socket = io(`${location.protocol}//${location.hostname}:8090`);
+socket.on('state', state =>
+  store.dispatch(setState(state))
+);
+
+const createStoreWithMiddleware = applyMiddleware(
+  remoteActionMiddleware(socket)
+)(createStore);
+
+const store = createStoreWithMiddleware(reducer);
+
+const routes = <Route component={App}>
+  <Route path="/results" component={ResultsContainer} />
+  <Route path="/" component={VotingContainer} />
+</Route>
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router history={hashHistory}>{routes}</Router>
+  </Provider>,
+  document.getElementById('app')
+)
